8d982e9f84c46d52d8aaf43cc8371cb5b2b135af
tsp93 <32462536+tsp93@users.noreply.github.com>
2019-01-22 18:01:20 +0000

    Whoa

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,7 @@
 module.exports = {
   extends: 'airbnb-base',
-  env: {
-    browser: true
-  },
   rules: {
     'no-console': ['error', { allow: ['info', 'warn', 'error'] }],
-    'class-methods-use-this': 0,
   },
   plugins: ['import'],
 };
